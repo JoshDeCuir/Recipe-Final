af8532ee75ddd81444b811905fe8f9464ab0e45c
JoshDeCuir <joshmdecuir@gmail.com>
2024-08-18 12:37:26 -0500

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://recipeapp-5531d183eed9.herokuapp.com/",
+    "https://recipeapp-5531d183eed9.herokuapp.com",
     "localhost",
     "127.0.0.1",
 ]
